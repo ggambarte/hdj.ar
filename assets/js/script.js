9b9f828f1a41863b451eef74858ce973f447a1ba
Gonzalo Gambarte <gonzalo_gambarte@hotmail.com>
2023-09-13 11:45:20 -0300

    Acualizaciones del contenido

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const elements = document.querySelectorAll(".element");
+const windowHeight = window.innerHeight;
+
+function checkElements() {
+    elements.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+        if (elementTop < windowHeight) {
+            element.classList.add("fade-in");
+        }
+    });
+}
+
+window.addEventListener("scroll", checkElements);
+
+
+const resizeButton = document.getElementById('resizeButton');
+
+function toggleSize() {
+    resizeButton.classList.toggle('active');
+}
+
+setInterval(toggleSize, 1000); // Cambia el tamaño cada 1000 ms (1 segundo)
